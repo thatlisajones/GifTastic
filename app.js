091e36d7e8c917dfac040818504f37c27cf5528a
That Lisa <thatlisajones@gmail.com>
2018-10-26 13:34:18 -0600

    Combine search elements into one div, tweak movie array

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-//Initial array of movies	
+//Create array of cinema search phrases	
 $(document).ready(function() {
 
-  var topics = ["Fritz Lang", "Who's Afraid of Virginia Woolf", "Sophia Loren", "Cabinet of Dr. Caligari", "La Dolce Vita", "Charles Laughton", "Buster Keaton", "Marlon Brando", "James Dean",  "The Passion of Joan of Arc", "Murnau Sunrise", "Double Indemnity"];
+  var topics = ["Fritz Lang", "Who's Afraid of Virginia Woolf", "Sophia Loren", "Cabinet of Dr. Caligari", "La Dolce Vita", "Charles Laughton", "Buster Keaton", "Marlon Brando", "James Dean",  "The Passion of Joan of Arc", "Murnau Sunrise", "Double Indemnity", "Wizard of Oz"];	
 
-  //  create topics array buttons
+  // Create buttons for items in array
   function renderButtons(){
     $('#buttons-view').empty();
 
@@ -18,47 +18,44 @@ $(document).ready(function() {
         }    
         renderButtons();
 
-//on button click
+// When a button is clicked...
 $(document).on('click', '.cinema', function() {
 
-    //new variable will log the text data from each button
     var cinemaSnob = $(this).html(); 
-    // console.log(cinemaSnob);
 
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + cinemaSnob + "&api_key=cBbCEaO8r8nfWDHs4ofmgngcz68DWzvy&limit=10";
-    // console.log(queryURL);
 
-    // Creating an AJAX call for the specific movie button being clicked
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).done(function(response) {
+  // Creating an AJAX call for the specific movie button being clicked
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(response) {
 
-      var results = response.data;
-        //console.log(results);
-        //empties the div before adding more gifs
-        $('#movies-view').empty();
-        for ( var j=0; j < results.length; j++) {
-                    var imageDiv = $('<div>');
-                    var imageView = results[j].images.fixed_height.url;
-                    var still = results[j].images.fixed_height_still.url;
-                        // console.log(imageView);  
+    var results = response.data;
+      //console.log(results);
+      //empties the div before adding more gifs
+      $('#movies-view').empty();
+      for ( var j=0; j < results.length; j++) {
+                  var imageDiv = $('<div>');
+                  var imageView = results[j].images.fixed_height.url;
+                  var still = results[j].images.fixed_height_still.url;
+                      // console.log(imageView);  
 
-        var gifImage = $('<img>').attr("src", still).attr('data-animate', imageView).attr('data-still', still);
-                    gifImage.attr('data-state', 'still');
-                    $('#movies-view').prepend(gifImage);
-                    gifImage.on('click', playGif);
+      var gifImage = $('<img>').attr("src", still).attr('data-animate', imageView).attr('data-still', still);
+                  gifImage.attr('data-state', 'still');
+                  $('#movies-view').prepend(gifImage);
+                  gifImage.on('click', playGif);
 
-        // Pulling ratings for each movie
-        var rating = results[j].rating;
-            // console.log(rating);
-        var displayRated= $('<p>').text("Rating: " + rating);
-        $('#movies-view').prepend(displayRated);
-  } // end for loop
+      // Pulling ratings for each movie
+      var rating = results[j].rating;
+          // console.log(rating);
+      var displayRated= $('<p>').text("Rating: " + rating);
+      $('#movies-view').prepend(displayRated);
+} // end for loop
 
-}); // done response
+});
 
-        //function to stop and animate gifs
+        //Still then animate
         function playGif() { 
                     var state = $(this).attr('data-state');
                     // console.log(state);
@@ -74,7 +71,7 @@ $(document).on('click', '.cinema', function() {
 
       }); //end of document on click 
 
-          //adding new button to array
+          //add new button to array
         $(document).on('click', '#add-movie', function(){
             if ($('#movie-input').val().trim() == ''){
               alert('Input can not be left blank');
